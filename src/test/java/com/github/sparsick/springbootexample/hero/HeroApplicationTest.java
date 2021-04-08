ae14060a840df7c1869a21c65cc62eb9034f8267
sw-dev1 <sw-dev@outlook.com>
2021-04-08 12:54:35 +0200

    Added Controller test. Added MvcTest with one response check.

--- src/test/java/com/github/sparsick/springbootexample/hero/HeroApplicationTest.java
+++ src/test/java/com/github/sparsick/springbootexample/hero/HeroApplicationTest.java
@@ -1,8 +1,12 @@
 package com.github.sparsick.springbootexample.hero;
 
 
+import com.github.sparsick.springbootexample.hero.universum.HeroController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class HeroApplicationTest {
@@ -11,4 +15,11 @@ public class HeroApplicationTest {
 	public void contextLoads() {
 	}
 
+	@Autowired
+	private HeroController controller;
+
+	@Test
+	public void controllerTest() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
